686eb5f6fbb9685c07317073dccf522941aae7d8
Yuto Urashima <yuto.urashima24@gmail.com>
2024-06-01 15:48:57 +0900

    chore: Remove unnecessary semicolons and comments in notes page code

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,8 +1,9 @@
-import { createClient } from '@/utils/supabase/server';
+import { createClient } from '@/utils/supabase/server'
 
+// TODO: supabaseのサンプルとして一時的に追加したため、必要がなくなったら削除する
 export default async function Notes() {
-  const supabase = createClient();
-  const { data: notes } = await supabase.from("notes").select();
+  const supabase = createClient()
+  const { data: notes } = await supabase.from('notes').select()
 
   return <p>{JSON.stringify(notes, null, 2)}</p>
 }
