043a12b2efed14dbec5310fbe30c0d2109357f22
Yuto Urashima <yuto.urashima24@gmail.com>
2024-06-02 17:10:12 +0900

    feat: Update validation for todoInputSchema to enforce a minimum length of 1 character for the title

--- src/schemas/todoInputForm/validation.ts
+++ src/schemas/todoInputForm/validation.ts
@@ -1,7 +1,7 @@
 import * as z from 'zod'
 
 export const todoInputSchema = z.object({
-  title: z.string().min(1)
+  title: z.string().min(1, { message: 'Todo must be at least 1 character long' })
 })
 
 export type TodoInputInferType = z.infer<typeof todoInputSchema>
