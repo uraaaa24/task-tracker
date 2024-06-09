7ee7d905aefaefb0aac3c8ded87825cf0c6fea2e
Yuto Urashima <yuto.urashima24@gmail.com>
2024-06-09 18:29:04 +0900

    feat: Add edit and delete buttons to ListItem component

--- src/components/elements/list/listItem.tsx
+++ src/components/elements/list/listItem.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable react/display-name */
+import { Button } from '@/components/ui/button'
 import { Checkbox } from '@/components/ui/checkbox'
+import { Pencil, Trash } from 'lucide-react'
 import { forwardRef } from 'react'
 
 export type Note = {
@@ -20,9 +22,19 @@ const ListItem = forwardRef<HTMLDivElement, ListItemProps>((props, ref) => {
   return (
     <div className="flex items-center space-x-2" ref={ref}>
       <Checkbox id={id} checked={props.value} onCheckedChange={props.onChange} />
-      <label htmlFor={id} className={`flex-1 ${props.value ? 'line-through text-gray-500' : ''} `}>
+      <label htmlFor={id} className={`${props.value ? 'line-through text-gray-500' : ''} `}>
         {title}
       </label>
+      <div className="space-x-1">
+        {/* 編集ボタン */}
+        <Button variant="ghost" onClick={() => console.log('Edit button clicked')} className="px-2">
+          <Pencil color="#6EE7B7" size={16} />
+        </Button>
+        {/* 削除ボタン */}
+        <Button variant="ghost" onClick={() => console.log('Delete button clicked')} className="px-2">
+          <Trash color="#F87171" size={16} />
+        </Button>
+      </div>
     </div>
   )
 })
